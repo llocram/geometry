4e1e4acd74aee5ef6b747e61784a4ae035783864
Marco Langer <langer.m86@gmail.com>
2021-06-23 21:02:44 +0200

    removed perf from project

--- perf/objdump/bezier.cpp
+++
@@ -1,9 +0,0 @@
-#include "../../include/geometry.h"
-
-using CubicBezier = geo::Bezier<3, geo::Vector3d, std::array<geo::Vector3d, 4>>;
-
-/* validate that the algorithm is SSE-vectorized */
-geo::Vector3d
-evaluate(CubicBezier const & bezier, double t) {
-  return geo::evaluateAt(bezier, t);
-}
--- perf/objdump/objdump.sh
+++
@@ -1,5 +0,0 @@
-g++ -std=c++20 -c bezier.cpp -o bezier_gcc.o -O2
-clang++ -std=c++20 -c bezier.cpp -o bezier_clang.o -O2
-
-objdump -d bezier_gcc.o > bezier_gcc_objdump
-objdump -d bezier_clang.o > bezier_clang_objdump
