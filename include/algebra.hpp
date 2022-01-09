112531b4ecdcbb58a9ba7150c152bafa6621e498
Marco Langer <langer.m86@gmail.com>
2022-01-09 22:00:32 +0100

    replaced C++17 style lambdas with C++20 style lambdas

--- include/algebra.hpp
+++ include/algebra.hpp
@@ -8,70 +8,99 @@ namespace geo {
 
 template <concepts::point Point>
 [[nodiscard]] constexpr Point
-operator-(Point const & lhs, Point const & rhs) noexcept {
-  return detail::substract_impl(
-    lhs, rhs, std::make_index_sequence<traits::dimension_v<Point>>());
+operator-(Point const & lhs, Point const & rhs) noexcept
+{
+  return [&]<std::size_t... Is>(std::index_sequence<Is...>)
+  {
+    Point retval;
+    (..., set<Point, Is>(retval, get<Point, Is>(lhs) - get<Point, Is>(rhs)));
+    return retval;
+  }(std::make_index_sequence<traits::dimension_v<Point>>{});
 }
 
 template <concepts::point Point>
 [[nodiscard]] constexpr Point
-operator+(Point const & lhs, Point const & rhs) noexcept {
-  return detail::addition_impl(
-    lhs, rhs, std::make_index_sequence<traits::dimension_v<Point>>());
+operator+(Point const & lhs, Point const & rhs) noexcept
+{
+  return [&]<std::size_t... Is>(std::index_sequence<Is...>)
+  {
+    Point retval;
+    (..., set<Point, Is>(retval, get<Point, Is>(lhs) + get<Point, Is>(rhs)));
+    return retval;
+  }(std::make_index_sequence<traits::dimension_v<Point>>{});
 }
 
 template <concepts::point Point, typename Scalar>
 requires concepts::value_type_equals<Point, Scalar>
 [[nodiscard]] constexpr Point
-operator*(Point const & point, Scalar scalar) noexcept {
-  return detail::multiply_impl(
-    point, scalar, std::make_index_sequence<traits::dimension_v<Point>>());
+operator*(Point const & point, Scalar scalar) noexcept
+{
+  return [&]<std::size_t... Is>(std::index_sequence<Is...>)
+  {
+    Point retval;
+    (..., set<Point, Is>(retval, get<Point, Is>(point) * scalar));
+    return retval;
+  }(std::make_index_sequence<traits::dimension_v<Point>>{});
 }
 
 template <concepts::point Point, typename Scalar>
 requires concepts::value_type_equals<Point, Scalar>
 [[nodiscard]] constexpr Point
-operator*(Scalar scalar, Point const & point) noexcept {
-  return detail::multiply_impl(
-    point, scalar, std::make_index_sequence<traits::dimension_v<Point>>());
+operator*(Scalar scalar, Point const & point) noexcept
+{
+  return operator*(point, scalar);
 }
 
 template <concepts::point Point, typename Scalar>
 requires concepts::value_type_equals<Point, Scalar>
 [[nodiscard]] constexpr Point
-operator/(Point const & point, Scalar scalar) noexcept(std::is_floating_point_v<Scalar>) {
-  return detail::division_impl(
-    point, scalar, std::make_index_sequence<traits::dimension_v<Point>>());
-}
-
-template <concepts::point Point1, concepts::point Point2>
-requires concepts::same_value_type<Point1, Point2>
-      && concepts::same_dimension<Point1, Point2>
-constexpr traits::value_type_t<Point1>
-dot_product(Point1 const & lhs, Point2 const & rhs) noexcept {
-  return detail::dot_product_impl(
-    lhs, rhs, std::make_index_sequence<traits::dimension<Point1>::value>());
+operator/(Point const & point, Scalar scalar) noexcept(std::floating_point<Scalar>)
+{
+  return [&]<std::size_t... Is>(std::index_sequence<Is...>)
+  {
+    if constexpr (std::integral<Scalar>) {
+      if (scalar == Scalar{}) {
+        throw std::runtime_error("division by zero");
+      }
+    }
+    Point retval;
+    (..., set<Point, Is>(retval, get<Point, Is>(point) / scalar));
+    return retval;
+  }(std::make_index_sequence<traits::dimension_v<Point>>{});
 }
 
 template <concepts::point Point>
-constexpr auto
-norm(Point const & point) {
+[[nodiscard]] constexpr traits::value_type_t<Point>
+dot_product(Point const & lhs, Point const & rhs) noexcept
+{
+  return [&]<std::size_t... Is>(std::index_sequence<Is...>)
+  {
+    return (... + (get<Point, Is>(lhs) * get<Point, Is>(rhs)));
+  }(std::make_index_sequence<traits::dimension_v<Point>>{});
+}
+
+template <concepts::point Point>
+[[nodiscard]] constexpr auto
+norm(Point const & point)
+{
   return sqrt(dot_product(point, point));
 }
 
 template <concepts::geo_object Geo1, concepts::geo_object Geo2>
 requires concepts::same_value_type<Geo1, Geo2>
-constexpr auto
-distance(Geo1 const & lhs, Geo2 const & rhs) noexcept {
+[[nodiscard]] constexpr auto
+distance(Geo1 const & lhs, Geo2 const & rhs) noexcept
+{
   return detail::distance_impl(
     lhs, rhs,
-    traits::tag_t<Geo1>(), traits::tag_t<Geo2>());
+    traits::tag_t<Geo1>{}, traits::tag_t<Geo2>{});
 }
 
 template <concepts::point Point>
 requires concepts::dimension_equals<Point, 3>
 [[nodiscard]] constexpr Point
-vector_product(Point const & lhs, Point const & rhs) noexcept {
+vector_product(Point const & lhs, Point const & rhs) noexcept
+{
   return Point(
       get<Point, 1>(lhs) * get<Point, 2>(rhs) - get<Point, 2>(lhs) * get<Point, 1>(rhs),
       get<Point, 2>(lhs) * get<Point, 0>(rhs) - get<Point, 0>(lhs) * get<Point, 2>(rhs),
@@ -79,8 +108,9 @@ vector_product(Point const & lhs, Point const & rhs) noexcept {
 }
 
 template <concepts::point Point>
-auto
-angle(Point const & lhs, Point const & rhs) noexcept (std::is_floating_point_v<traits::value_type_t<Point>>){
+[[nodiscard]] auto
+angle(Point const & lhs, Point const & rhs) noexcept (std::is_floating_point_v<traits::value_type_t<Point>>)
+{
   using value_type = traits::value_type_t<Point>;
 
   auto const normProduct = norm(lhs) * norm(rhs);
--- include/detail/detail_algebra.hpp
+++ include/detail/detail_algebra.hpp
@@ -13,62 +13,12 @@ namespace geo {
 
 namespace detail {
 
-template <concepts::point Point1, concepts::point Point2>
-requires concepts::same_value_type<Point1, Point2>
-      && concepts::same_dimension<Point1, Point2>
+template <concepts::point Point>
 constexpr auto
 distance_impl(
-    Point1 const & lhs, Point2 const & rhs,
+    Point const & lhs, Point const & rhs,
     traits::point_tag, traits::point_tag) noexcept {
   return norm(lhs - rhs);
-}
-
-template <concepts::point Point, std::size_t... I>
-constexpr Point
-substract_impl(Point const & lhs, Point const & rhs, std::index_sequence<I...> const &) noexcept {
-  Point retval;
-  (..., set<Point, I>(retval, get<Point, I>(lhs) - get<Point, I>(rhs)));
-  return retval;
-}
-
-template <concepts::point Point, std::size_t... I>
-constexpr Point
-addition_impl(Point const & lhs, Point const & rhs, std::index_sequence<I...> const &) noexcept {
-  Point retval;
-  (..., set<Point, I>(retval, get<Point, I>(lhs) + get<Point, I>(rhs)));
-  return retval;
-}
-
-template <concepts::point Point, typename Scalar, std::size_t... I>
-requires concepts::value_type_equals<Point, Scalar>
-constexpr Point
-multiply_impl(Point const & lhs, Scalar scalar, std::index_sequence<I...> const &) noexcept {
-  Point retval;
-  (..., set<Point, I>(retval, get<Point, I>(lhs) * scalar));
-  return retval;
-}
-
-template <concepts::point Point, typename Scalar, std::size_t... I>
-requires concepts::value_type_equals<Point, Scalar>
-constexpr Point
-division_impl(Point const & lhs, Scalar scalar, std::index_sequence<I...> const &) noexcept(std::is_floating_point_v<Scalar>) {
-  if constexpr (std::is_integral_v<Scalar>) {
-    if (scalar == Scalar()) {
-      throw std::runtime_error("division by zero");
-    }
-  }
-  Point retval;
-  (..., set<Point, I>(retval, get<Point, I>(lhs) / scalar));
-  return retval;
-}
-
-template <concepts::point Point1, concepts::point Point2, std::size_t... I>
-requires concepts::same_value_type<Point1, Point2>
-      && concepts::same_dimension<Point1, Point2>
-constexpr traits::value_type_t<Point1>
-dot_product_impl(
-    Point1 const & lhs, Point2 const & rhs, std::index_sequence<I...> const &) noexcept {
-  return (... + (get<Point1, I>(lhs) * get<Point2, I>(rhs)));
 }
 
 } // namespace detail
